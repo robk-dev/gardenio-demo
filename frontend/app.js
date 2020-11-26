f390607891edc077429e00d9d48922a86a82c7fb
Robert Kardjaliev <robk-dev@users.noreply.github.com>
2020-11-26 21:50:44 +0000

    connecting to keycloak from js server

--- frontend/app.js
+++ frontend/app.js
@@ -6,6 +6,31 @@ const backendServiceEndpoint = `http://backend/hello-backend`
 
 app.get('/hello-frontend', (req, res) => res.send('Hello from the frontend!'));
 
+app.get('/call-keycloak', async (_req, res) => {
+  try {
+    const tokens = await request.post({
+      url: 'http://keycloak-http:80/auth/realms/master/protocol/openid-connect/token',
+      form: {
+        "client_id": "admin-cli",
+        "username": "admin",
+        "password": "admin",
+        "grant_type": "password"
+      }
+    });
+
+    const { access_token } = JSON.parse(tokens);
+    const getMasterRealm = await request.get({
+      url: 'http://keycloak-http:80/auth/admin/realms/master',
+      headers: { Authorization: `Bearer ${access_token}` }
+    });
+
+    console.log({ getMasterRealm });
+    res.send({ access_token, getMasterRealm });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 app.get('/call-backend', (req, res) => {
   // Query the backend and return the response
   request.get(backendServiceEndpoint)
